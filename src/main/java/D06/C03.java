55e89bd602acfd40f48453a4817e018eb76c18ce
demnuri <demircii.nurullah@gmail.com>
2025-09-22 14:24:41 +0300

    version3

--- src/main/java/D06/C03.java
+++ src/main/java/D06/C03.java
@@ -1,8 +1,26 @@
 package D06;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utilities.TestBaseBeforeAfter;
 
 public class C03 extends TestBaseBeforeAfter {
+
+    //file exist upload
+
+    @Test
+    public void test(){
+
+        driver.get("https://the-internet.herokuapp.com/upload");
+        WebElement dosyaSec = driver.findElement(By.xpath("//*[@id='file-upload']"));
+        String uploaded = "C:\\Users\\testinium\\OneDrive\\Masaüstü\\SampleFile.txt";
+        dosyaSec.sendKeys(uploaded);
+        driver.findElement(By.xpath("//*[@id='file-submit']")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//h3")).isDisplayed());
+
+    }
 
 
 }
